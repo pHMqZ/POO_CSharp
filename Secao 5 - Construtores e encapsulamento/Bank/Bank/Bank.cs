81fc6118f713532deb6c6cec5763943a10a29300
Phillip Marques <phillip.silva@capgemini.com>
2022-07-08 19:14:03 -0300

    exercicio de fixacao - parte 1

---
+++ Secao 5 - Construtores e encapsulamento/Bank/Bank/Bank.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Bank {
+    class Bank {
+
+        public int Numero { get; private set; }
+        public String Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        public Bank(int numero, string titular) {
+            Numero = numero;
+            Titular = titular;
+        }
+
+        public Bank (int numero, string titular, double saldo) : this(numero, titular) {
+            Saldo = saldo;
+        }
+
+        public override string ToString() {
+            return "Conta "
+                + Numero
+                + ", Titular"
+                + ", Saldo: $ "
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Secao 5 - Construtores e encapsulamento/Bank/Bank/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Bank {
+    class Program {
+        static void Main(string[] args) {
+
+            Bank conta;
+            
+            Console.Write("Entre o número da conta: ");
+            int num = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char resp = char.Parse(Console.ReadLine());
+
+            if (resp == 's' || resp == 'S') {
+                Console.Write("Entre o valor de depósito inicial: ");
+                double depInical = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new Bank(num, titular, depInical);
+            } else {
+                conta = new Bank(num, titular);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(conta);
+        }
+    }
+}
