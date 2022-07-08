07478249e3d6ba9145e8adb00512b0c48bc72bdb
Phillip Marques <phillip.silva@capgemini.com>
2022-07-08 17:23:47 -0300

    initial commit

---
+++ Secao 4/ClassesEMetodos/ImplemantandoOO/ImplemantandoOO/Triangulo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImplemantandoOO {
+    class Triangulo {
+
+        public double A;
+        public double B;
+        public double C;
+
+    }
+}
