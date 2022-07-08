6c784689ce2693b13ba61c70ebe9471fb63e8c97
Phillip Marques <phillip.silva@capgemini.com>
2022-07-08 17:46:10 -0300

    Criando métodos

--- Secao 4/ClassesEMetodos/ImplemantandoOO/ImplemantandoOO/Program.cs
+++ Secao 4/ClassesEMetodos/ImplemantandoOO/ImplemantandoOO/Program.cs
@@ -19,11 +19,10 @@ namespace ImplemantandoOO {
             y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            double p = (x.A + x.B + x.C) / 2.0;
-            double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
 
-            p = (y.A + y.B + y.C) / 2.0;
-            double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+            double areaX = x.Area();
+
+            double areaY = y.Area();
 
             Console.WriteLine("Área de x = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
             Console.WriteLine("Área de y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
--- Secao 4/ClassesEMetodos/ImplemantandoOO/ImplemantandoOO/Triangulo.cs
+++ Secao 4/ClassesEMetodos/ImplemantandoOO/ImplemantandoOO/Triangulo.cs
@@ -9,5 +9,10 @@ namespace ImplemantandoOO {
         public double B;
         public double C;
 
+        public double Area () {
+            double p = (A + B + C) / 2.0;
+            return Math.Sqrt(p * (p-A)*(p-B)*(p-C));
+            
+        }
     }
 }
