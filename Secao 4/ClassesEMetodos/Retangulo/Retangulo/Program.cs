dc3b9e63b052d29a04c99de7a2b99b87f88a3c75
Phillip Marques <phillip.silva@capgemini.com>
2022-07-08 18:31:55 -0300

    Exercicios 2

---
+++ Secao 4/ClassesEMetodos/Retangulo/Retangulo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace Retangulo {
+    class Program {
+        static void Main(string[] args) {
+
+            Retangulo r = new Retangulo();
+
+            Console.WriteLine("Entre a largura e altura do retângulo: ");
+            r.Largura = double.Parse(Console.ReadLine());
+            r.Altura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("AREA = " + r.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERIMETRO = " + r.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL = " + r.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ Secao 4/ClassesEMetodos/Retangulo/Retangulo/Retangulo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Retangulo {
+    class Retangulo {
+
+        public double Largura;
+        public double Altura;
+
+        public double Area() {
+            return Altura * Largura;
+        }
+
+        public double Perimetro() {
+            return (Largura + Altura) * 2;
+        }
+
+        public double Diagonal() {
+            return Math.Sqrt(Largura * Largura + Altura + Altura);
+
+        }
+    }
+}
