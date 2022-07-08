0e8e03e3c55334e5d021c7a896bda1bc8762acdd
Phillip Marques <phillip.silva@capgemini.com>
2022-07-08 17:34:48 -0300

    Primeiros exercicios

---
+++ Secao 4/ClassesEMetodos/PessoaMaisVelha/PessoaMaisVelha/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PessoaMaisVelha {
+    class Program {
+        static void Main(string[] args) {
+
+            Pessoa p1, p2;
+
+            p1 = new Pessoa();
+            p2 = new Pessoa();
+
+            Console.WriteLine("Dados da primeira pessoa: ");
+            Console.WriteLine("Nome: ");
+            p1.nome = Console.ReadLine();
+            Console.WriteLine("Idade: ");
+            p1.idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados da segunda pessoa: ");
+            Console.WriteLine("Nome: ");
+            p2.nome = Console.ReadLine();
+            Console.WriteLine("Idade: ");
+            p2.idade = int.Parse(Console.ReadLine());
+
+            if (p1.idade > p2.idade) {
+                Console.WriteLine("Pessoa mais velha: " + p1.nome);
+            } else {
+                Console.WriteLine("Pessoa mais velha: " + p2.nome);
+            }
+        }
+    }
+}
