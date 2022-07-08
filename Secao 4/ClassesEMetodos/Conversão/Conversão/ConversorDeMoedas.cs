c423621f17defb820a9c9632aeff78368983426b
Phillip Marques <phillip.silva@capgemini.com>
2022-07-08 18:43:55 -0300

    Exercicio de fixacao

---
+++ Secao 4/ClassesEMetodos/Conversão/Conversão/ConversorDeMoedas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Conversão {
+    class ConversorDeMoedas {
+
+        public static double IoF = 6.0;
+
+        public static double DolarParaReal(double quantia, double cotacao) {
+            double total = quantia * cotacao;
+            return total += total * IoF / 100.0;
+        } 
+    }
+}
